b7933307d3f8767d79f4407bec409bfa59621aeb
K0ban4ik7089 <borissviridov24@gmail.com>
2026-01-13 17:30:36 +0700

    Созданы основные папки и модели

--- obj/Debug/net8.0/CollegeSchedule.AssemblyInfo.cs
+++ obj/Debug/net8.0/CollegeSchedule.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CollegeSchedule")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e873893b33ee04c3d3547260a2c8d71f271f351")]
 [assembly: System.Reflection.AssemblyProductAttribute("CollegeSchedule")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CollegeSchedule")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
